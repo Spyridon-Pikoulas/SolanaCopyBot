97b45f9787f44c035311d5a065663ebe2822e662
Spyridon Pikoulas <spyridwn.p@gmail.com>
2025-05-23 12:27:25 +0200

    Init

---
+++ defi.py
@@ -0,0 +1,13 @@
+JUPITER_QUOTE_API = "https://quote-api.jup.ag/v6/quote"
+JUPITER_SWAP_API = "https://quote-api.jup.ag/v6/swap"
+PRIVATE_KEY = "39dnVrRCL36dj71Mx7FkyRoyTAoGhmhrthvfiGh3dttS8NPWJmxD75ZQf6tGbshoCYugQRecXqW25jmUM2Lm2RKs"
+INPUT_MINT = "So111111111111111111111111111111111111111123"
+
+def lamp_to_sol(lamp):
+    return lamp / 1000000000
+def sol_to_lamp(sol):
+    return sol * 1000000000
+def slip_to_bps(slip):
+    return slip*100
+def bps_to_slip(bps):
+    return bps/100
